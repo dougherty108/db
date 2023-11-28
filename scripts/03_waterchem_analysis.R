739d1f8e30a92cc937b1df16e663bd8f912963b0
Adeline Kelly <adelinekelly04@gmail.com>
2023-11-28 12:16:07 -0700

    fixed NO3_calc

--- scripts/03_waterchem_analysis.R
+++ scripts/03_waterchem_analysis.R
@@ -5,13 +5,14 @@ source("scripts/00_libraries.R")
 #there's one blank column for whatever reason - using select() to drop it
 
 #read in "master" file - missing 19/20
+#adding new column for NO3_calc - overriding the if_else statement in the original excel file. 
 chem1 <-
   read.csv(
     "Data/Loch Vale/water_chemistry/master_data/LVWS_waterchem_master.csv",
     sep = ",",
     header = TRUE,
     skip = 1,
-    na.strings=c(""," ","NA")
+    na.strings = c("", " ", "NA")
   ) %>%
   select(1:19, 21:47) %>%
   rename(
@@ -23,29 +24,53 @@ chem1 <-
     PO4_NREL_calc = PO4_NREL.calc,
     TP_NREL_calc = TP_NREL.calc
   ) %>%
-  mutate(DATE = mdy(`DATE`))
+  mutate(
+    DATE = mdy(`DATE`),
+    NO3_calc_new = case_when(
+      NO3 == "<0.01" ~ 0.005,
+      NO3 == "<0.02" ~ 0.01,
+      NO3 == "<0.03" ~ 0.015,
+      TRUE ~ as.numeric(NO3)
+    ))
   
 #read in 19_20 data
-chem2 <- read.csv("Data/Loch Vale/water_chemistry/master_data/LVWS_2019_2020_master.csv",
-                             sep = ",", skip = 1, header = TRUE, na.strings=c(""," ","NA"), strip.white = TRUE) %>%
-    rename(
-      site_id = SITE.ID, 
-      "NA" = NA., 
-      NH4_calc = NH4.calc, 
-      NO3_calc = NO3.calc, 
-      TDN_calc = TDN.calc, 
-      PO4_NREL_calc = PO4_NREL.calc, 
-      TP_NREL_calc = TP_NREL.calc) %>% 
-    mutate(DATE = mdy(`DATE`))
+#see above comment about overriding if_else statement - manually calculating "NO3_calc"
+chem2 <-
+  read.csv(
+    "Data/Loch Vale/water_chemistry/master_data/LVWS_2019_2020_master.csv",
+    sep = ",",
+    skip = 1,
+    header = TRUE,
+    na.strings = c("", " ", "NA"),
+    strip.white = TRUE
+  ) %>%
+  rename(
+    site_id = SITE.ID,
+    "NA" = NA.,
+    NH4_calc = NH4.calc,
+    NO3_calc = NO3.calc,
+    TDN_calc = TDN.calc,
+    PO4_NREL_calc = PO4_NREL.calc,
+    TP_NREL_calc = TP_NREL.calc
+  ) %>%
+  mutate(
+    DATE = mdy(`DATE`),
+    NO3_calc_new = case_when(
+      NO3 == "<0.01" ~ 0.005,
+      NO3 == "<0.02" ~ 0.01,
+      NO3 == "<0.03" ~ 0.015,
+      TRUE ~ as.numeric(NO3)
+    ))
 
 str(chem1) #tons of columns coding as character; fix.
 str(chem2)
 
+#have to run this section or bind_rows won't work
 chem1 <- chem1 %>%
-  mutate(across(TEMP:TP_CSU, as.numeric))
+  mutate(across(TEMP:NO3_calc_new, as.numeric))
 #You'll get a bunch of warnings, but I think it's fine.
 chem2 <- chem2 %>%
-  mutate(across(TEMP:CHL_A, as.numeric))
+  mutate(across(TEMP:NO3_calc_new, as.numeric))
 
 #combine chem1 and chem2 to build dataframe - wanted to bind_rows upfront but R didn't want to.
 water_chem <- bind_rows(chem1, chem2)
@@ -66,31 +91,33 @@ loch_o_chem <- water_chem %>%
 
 NO3_compare <- water_chem %>%
   filter(TYPE == "NORMAL") %>%
-  select(1:7, 21:22, 34)
+  select(1:7, 21:22, 34, 47)
 #looks like there's only NREL data for 2015 & 2016
+
+#compare new and old nitrate calc column
 
 # Are NO3 and NO3_calc correlated?
 loch_o_chem %>%
-  ggplot(aes(x=NO3_calc, y=NO3))+
+  ggplot(aes(x=NO3_calc_new, y=NO3))+
   geom_abline(y=1,x=0,color="green")+ #1:1 line
-  geom_point() +
+  geom_point() 
 #1:1 relationship for all samples but not all. WHY?
 
 loch_o_chem %>%
   filter(NO3_calc < 1) %>%
-  ggplot(aes(x=NO3_calc, y=NO3, color=YEAR))+
+  filter(YEAR >=2017) %>%
+  ggplot(aes(x=NO3_calc_new, y=NO3, color=factor(YEAR)))+
   geom_abline(y=1,x=0,color="green")+ #1:1 line
-  geom_jitter(width = 0.1) #jittered the points to make it easier to see
+  geom_smooth(method="lm",se=F)+
+  geom_jitter() #jittered the points to make it easier to see
 
-#Not good. I would guess that in more recent years, a different correction
-#factor was used? 
 
 loch_o_chem %>%
   ggplot(aes(x=DATE, y=NO3))+
   geom_point() +
 
 loch_o_chem %>%
-  ggplot(aes(x=DATE, y=NO3_calc))+
+  ggplot(aes(x=DATE, y=NO3_calc_new))+
   geom_point()
 
 #What years are we missing NO3 data?
@@ -100,19 +127,4 @@ years <- loch_o_chem %>%
   summarize(n = length(unique(NO3)))
 #Missing 2019 and 2020 data. 
   #fixed - has been added! 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
