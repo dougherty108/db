11ee6c1c99e0a92356b834a297e19d1bff9fa3f0
Adeline Kelly <adelinekelly04@gmail.com>
2024-01-25 15:49:57 -0700

    export csv for all fern chla data, plot

--- scripts/xx_chla_db.R
+++ scripts/xx_chla_db.R
@@ -9,11 +9,21 @@ fern_chla <- read.csv("Data/Loch Vale/water_chemistry/chlorophyll/lvws_chlorophy
   rename(chla_final = 13) %>%
   write_csv("fern_chla.csv", col_names = TRUE)
 
+fern_chla_all <- read.csv("Data/Loch Vale/water_chemistry/chlorophyll/lvws_chlorophyll.csv", header = TRUE) %>%
+  filter(lake == "FERN") %>%
+  select(1:13) %>%
+  mutate(date_sample = mdy(date_sample)) %>%
+  rename(chla_final = 13) %>%
+  write_csv("fern_chla_all.csv", col_names = TRUE)
 
+fchla_db <- fern_chla_all %>%
+  mutate(sample_ID = as.factor(sample_ID)) %>%
+  mutate(chla_final = as.numeric(chla_final))
 
-
-
-
+fchla_db %>%
+  ggplot(aes(x = date_sample, y = chla_final)) +
+  geom_point() +
+  facet_grid(~ sample_ID)
 
 
 
