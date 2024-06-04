256f14da8213018162e029a208ad82bbc55928ff
Adeline Kelly <adelinekelly04@gmail.com>
2024-06-04 17:35:09 -0600

    fixed post merging issues

--- scripts/00_libraries.R
+++ scripts/00_libraries.R
@@ -24,16 +24,8 @@ pacman::p_load("lubridate",
                "huxtable",
                "ggridges",
                "viridis",
-<<<<<<< HEAD
-<<<<<<< HEAD
                "ggthemes",
                "huxtable")
-=======
-               "ggthemes")
->>>>>>> 8f3f8c2ba9148423c5b4a79c20b888cf77e047f1
-=======
-               "ggthemes")
->>>>>>> 8f3f8c2ba9148423c5b4a79c20b888cf77e047f1
 
 #Use renv for version control.  Beginner guide here:
 # https://rstudio.github.io/renv/articles/renv.html
