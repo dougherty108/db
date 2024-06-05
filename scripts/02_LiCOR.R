366f37f8fdff6939599069afb28ec24e2b09ddc8
Adeline Kelly <adelinekelly04@gmail.com>
2024-06-04 18:05:36 -0600

    placeholder script for LiCOR

---
+++ scripts/02_LiCOR.R
@@ -0,0 +1,37 @@
+source("scripts/00_libraries.R")
+
+# placeholder script for LiCOR profiles. I'll see if I have any sample data
+  # that I can use to write a skeleton script from -AGK
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
