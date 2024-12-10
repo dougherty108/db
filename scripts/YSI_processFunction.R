f6d4e6fa14f892bab17d9104c0b3fcec78ebb390
adelinekelly <adelinekelly04@gmail.com>
2024-12-10 11:14:41 -0700

    Merge date and time columns, fix date formatting, return data in long format

--- scripts/YSI_processFunction.R
+++ scripts/YSI_processFunction.R
@@ -1,23 +1,16 @@
 source("scripts/00_libraries.R")
 
-# need to write columns with site name, etc. from file path
-# merge date and time columns
-
-# processes YSI data - reads in file, renames columns, selects desired columns, returns new dataframe
 process_ysi <- function(file_path) {
-  
   # Extract information from file name
   file_name <- path_file(file_path)
   file_info <- strsplit(file_name, "[_]")[[1]]
-  
   # Read in file
   data <- read.csv(file_path, sep = ",", header = TRUE, skip = 18, skipNul = TRUE, check.names = FALSE)
-  
   # Fix encoding and special characters in column names
   Encoding(colnames(data)) <- "latin1"
   colnames(data) <- gsub("<b5>", "µ", colnames(data))
-  
-  # Rename and select columns
+  # Rename columns
+    # Call by column name rather than position
   data <- data %>%
     rename(
       date = "Date (MM/DD/YYYY)", 
@@ -34,38 +27,30 @@ process_ysi <- function(file_path) {
       temp = "Temp °C", 
       barometer = "Barometer mmHg"
     ) %>%
-    select(date, time, chla, cond, depth, do_percent, do_mgL, orp, specific_cond, 
+    # Merge date and time columns
+    mutate(date_time = paste(date, time)) %>%
+    # Select desired columns
+    select(date_time, chla, cond, depth, do_percent, do_mgL, orp, specific_cond, 
       TAL_PC, pH, temp, barometer
     ) %>%
+    # Create columns for lake and site from file path
     mutate(lake = file_info[1]) %>%
     mutate(site = file_info[2]) %>%
-    relocate(lake, .before = date) %>%
-    relocate(site, .before = date) %>%
-    mutate(date = mdy(date)) %>%
-    mutate(time = hms(time))
-  
+    # Move columns
+    relocate(date_time, .before = chla) %>%
+    relocate(lake, .before = date_time) %>%
+    relocate(site, .before = date_time) %>%
+    relocate(depth, .after = date_time) %>%
+    # Fix date formatting
+    mutate(date_time = mdy_hms(date_time)) %>%
+    # Pivot to long format
+    pivot_longer(cols = c(chla:barometer), names_to = "paramter") %>%
+  # Return new dataframe
   return(data)
 }
 
-
-
-# example with Andrew's Tarn below
-result_df <- process_ysi("Data/LVWS/01_YSI/AND_TARN/raw/AndrewsTarn_Outlet_20240924.csv")
-
-# figure out pulling info from file name 
-
-file_name <- path_file("Data/LVWS/01_YSI/AND_TARN/raw/AndrewsTarn_Outlet_20240924.csv")
-file_info <- strsplit(file_name, "[_]")[[1]]
-
-# date and time need to be parsed, relocate lake and site
-
-
-
-
-
-
-
-
+# Example below
+result_df <- process_ysi("Data/On Thin Ice/01_YSI/LOC/raw/Loch_Inlet_20240709.csv")
 
 
 
