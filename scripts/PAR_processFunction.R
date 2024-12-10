a15060a7c11f7bfa54c3f4456afeb7f719a1c3c2
adelinekelly <adelinekelly04@gmail.com>
2024-12-10 11:46:38 -0700

    minor edits - cleaned, commented, etc etc

--- scripts/PAR_processFunction.R
+++ scripts/PAR_processFunction.R
@@ -5,32 +5,30 @@ process_par <- function(file_path) {
   # Extract information from file name
   file_name <- path_file(file_path)
   file_info <- strsplit(file_name, "[_/]")[[1]]
-  
-  # Read in data from CSV file
+  # Read in file
   data <- read.csv(file_path, sep = ",", skip = 8, header = FALSE, strip.white = TRUE)
-  
-  # Rename and select columns
+  # Rename columns
   data <- data %>%
   rename("scan_number" = 1, "date" = 2, "time" = 3, "raw_value" = 4, "calibrated_value" = 5) %>%
+  # Create date_time column, fix date formatting
   mutate(date_time = paste(date, time)) %>%
   mutate(date_time = dmy_hms(date_time)) %>%
-  relocate(date_time, .before = raw_value) %>%
+  # Select columns
   select(date_time, raw_value, calibrated_value) %>%
+  # Create columns for site and depth from file name
   mutate(lake = file_info[1]) %>%
   mutate(depth = file_info[2]) %>%
-  mutate(depth_from = file_info[3])
-  
+  mutate(depth_from = file_info[3]) %>%
+  # Move columns
+  relocate(lake, .before = date_time) %>%
+  relocate(depth, .after = date_time) %>%
+  relocate(depth_from, .after = depth)
+  # Return new dataframe
   return(data)
   
 }
 
-result <- process_par("Data/On Thin Ice/07_PARsensors/LOC/raw/LOC_1.5_BOT_20240815_20241017.CSV")
-
-# Write to new CSV file - include as part of function? 
-# Need to think about file naming - name CSV file from file path? 
-
-# Visualize PAR data
-
+result_df <- process_par("Data/On Thin Ice/07_PARsensors/LOC/raw/LOC_1.5_BOT_20240815_20241017.CSV")
 
 
 
