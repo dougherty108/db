6498a8d196c1063ca87130ff37838764dc64c247
bellaoleksy <bellaoleksy@gmail.com>
2025-06-03 13:13:41 -0600

    get a skeleton in place to start exporting profiles for OTI project

--- scripts/01_compile_YSI.R
+++ scripts/01_compile_YSI.R
@@ -1,5 +1,203 @@
-source("scripts/00_libraries.R")
-source("scripts/00_functions.R")
+source("functions/00_libraries.R")
+source("functions/00_helper_functions.R")
+source("functions/01_ysi_profile.R")
+
+# Profiles for Green Lake 4 (GL4) ---------------------------------------------
+
+# Inspect the profiles, summarize the data, and export into "GL4 > export" folder
+
+GL4_dir <- here("data/Sensors/YSI Pro DSS/GL4/raw")
+
+# Get all text files in the main directory and its subdirectories
+GL4files <- dir_ls(GL4_dir, regexp = "\\.csv$", recurse = TRUE)
+
+# Only look at the "Zmax" files
+GL4files <- GL4files[str_detect(GL4files, "Zmax")]
+length(GL4files)
+
+
+# ... 2024-06-27 ----------------------------------------------------------
+
+
+GL4_1 <- process_ysi(GL4files[1])
+head(GL4_1)
+
+GL4_1 %>%
+  mutate(depth_m=round(depth_m/0.5)*0.5) %>% #round to the nearest 0.5
+  group_by(depth_m, parameter, lake) %>%
+  mutate(value = median(value, na.rm=TRUE)) %>%
+  mutate(month=month(date_time)) %>%
+  filter(!parameter %in% c("barometer_mmHg","cond_spec_uScm")) %>%
+  ggplot(aes(x=value, y=depth_m, color=parameter))+
+  geom_point()+
+  scale_y_reverse()+
+  facet_wrap(parameter~., scales="free_x", nrow = 2)+
+  labs(title=paste(unique(GL4_1$lake),unique(GL4_1$date)))
+
+#Export a CSV with rounded depths
+GL4_20240627 <- GL4_1 %>%
+  mutate(depth_m=round(depth_m/0.5)*0.5) %>% #round to the nearest 0.5
+  group_by(lake, date, depth_m, parameter) %>%
+  summarize(value = median(value, na.rm=TRUE)) 
+write_csv(GL4_20240627, here("data/Sensors/YSI Pro DSS/GL4/export/GL4_20240627.csv"))
+
+# ... 2024-07-23 ----------------------------------------------------------
+
+GL4_2 <- process_ysi(GL4files[2])
+head(GL4_2)
+
+GL4_2 %>%
+  mutate(depth_m=round(depth_m/0.5)*0.5) %>% #round to the nearest 0.5
+  group_by(depth_m, parameter, lake) %>%
+  mutate(value = median(value, na.rm=TRUE)) %>%
+  mutate(month=month(date_time)) %>%
+  filter(!parameter %in% c("barometer_mmHg","cond_spec_uScm")) %>%
+  ggplot(aes(x=value, y=depth_m, color=parameter))+
+  geom_point()+
+  scale_y_reverse()+
+  facet_wrap(parameter~., scales="free_x", nrow = 2)+
+  labs(title=paste(unique(GL4_2$lake),unique(GL4_2$date)))
+
+#Export a CSV with rounded depths
+
+# ... 2024-08-22 ----------------------------------------------------------
+
+GL4_3 <- process_ysi(GL4files[3])
+head(GL4_3)
+
+GL4_3 %>%
+  mutate(depth_m=round(depth_m/0.25)*0.25) %>% #round to the nearest 0.5
+  group_by(depth_m, parameter, lake) %>%
+  mutate(value = median(value, na.rm=TRUE)) %>%
+  mutate(month=month(date_time)) %>%
+  filter(!parameter %in% c("barometer_mmHg","cond_spec_uScm")) %>%
+  ggplot(aes(x=value, y=depth_m, color=parameter))+
+  geom_point()+
+  scale_y_reverse()+
+  facet_wrap(parameter~., scales="free_x", nrow = 2)+
+  labs(title=paste(unique(GL4_3$lake),unique(GL4_3$date)))
+
+#Export a CSV with rounded depths
+
+# ... 2024-09-24 ----------------------------------------------------------
+
+GL4_4 <- process_ysi(GL4files[4])
+head(GL4_4)
+
+GL4_4 %>%
+  mutate(depth_m=round(depth_m/0.25)*0.25) %>% #round to the nearest 0.5
+  group_by(depth_m, parameter, lake) %>%
+  mutate(value = median(value, na.rm=TRUE)) %>%
+  mutate(month=month(date_time)) %>%
+  filter(!parameter %in% c("barometer_mmHg","cond_spec_uScm")) %>%
+  ggplot(aes(x=value, y=depth_m, color=parameter))+
+  geom_point()+
+  scale_y_reverse()+
+  facet_wrap(parameter~., scales="free_x", nrow = 2)+
+  labs(title=paste(unique(GL4_4$lake),unique(GL4_4$date)))
+
+#Export a CSV with rounded depths
+
+# Profiles for The Loch (LOC) ---------------------------------------------
+
+# Inspect the profiles, summarize the data, and export inot "LOC > export" folder
+
+LOC_dir <- here("data/Sensors/YSI Pro DSS/LOC/raw")
+
+# Get all text files in the main directory and its subdirectories
+LOCfiles <- dir_ls(LOC_dir, regexp = "\\.csv$", recurse = TRUE)
+
+# Only look at the "Zmax" files
+LOCfiles <- LOCfiles[str_detect(LOCfiles, "Zmax")]
+length(LOCfiles)
+
+# ... 2024-03-05 ----------------------------------------------------------
+
+LOC1 <- process_ysi(LOCfiles[1])
+head(LOC1)
+
+LOC1 %>%
+  mutate(depth_m=round(depth_m/0.25)*0.25) %>% #round to the nearest 0.5
+  group_by(depth_m, parameter, lake) %>%
+  mutate(value = median(value, na.rm=TRUE)) %>%
+  mutate(month=month(date_time)) %>%
+  filter(!parameter %in% c("barometer_mmHg","cond_spec_uScm")) %>%
+  ggplot(aes(x=value, y=depth_m, color=parameter))+
+  geom_point()+
+  scale_y_reverse()+
+  facet_wrap(parameter~., scales="free_x", nrow = 2)+
+  labs(title=paste(unique(LOC1$lake),unique(LOC1$date)))
+
+
+# ... 2024-04-23 ----------------------------------------------------------
+
+LOC2 <- process_ysi(LOCfiles[2])
+head(LOC2)
+
+LOC2 %>%
+  mutate(depth_m=round(depth_m/0.25)*0.25) %>% #round to the nearest 0.5
+  group_by(depth_m, parameter, lake) %>%
+  mutate(value = median(value, na.rm=TRUE)) %>%
+  mutate(month=month(date_time)) %>%
+  filter(!parameter %in% c("barometer_mmHg","cond_spec_uScm")) %>%
+  ggplot(aes(x=value, y=depth_m, color=parameter))+
+  geom_point()+
+  scale_y_reverse()+
+  facet_wrap(parameter~., scales="free_x", nrow = 2)+
+  labs(title=paste(unique(LOC2$lake),unique(LOC2$date)))
+
+
+# ... 2024-05-13 ----------------------------------------------------------
+
+LOC3 <- process_ysi(LOCfiles[3])
+head(LOC3)
+
+LOC3 %>%
+  mutate(depth_m=round(depth_m/0.25)*0.25) %>% #round to the nearest 0.5
+  group_by(depth_m, parameter, lake) %>%
+  mutate(value = median(value, na.rm=TRUE)) %>%
+  mutate(month=month(date_time)) %>%
+  filter(!parameter %in% c("barometer_mmHg","cond_spec_uScm")) %>%
+  ggplot(aes(x=value, y=depth_m, color=parameter))+
+  geom_point()+
+  scale_y_reverse()+
+  facet_wrap(parameter~., scales="free_x", nrow = 2)+
+  labs(title=paste(unique(LOC3$lake),unique(LOC3$date)))
+
+
+# ... For dates with multiple holes, pick the best looking one and export that ----------------------------------------------------------
+LOC14 <- process_ysi(LOCfiles[14])
+head(LOC14)
+
+LOC14 %>%
+  mutate(depth_m=round(depth_m/0.25)*0.25) %>% #round to the nearest 0.5
+  group_by(depth_m, parameter, lake) %>%
+  mutate(value = median(value, na.rm=TRUE)) %>%
+  mutate(month=month(date_time)) %>%
+  filter(!parameter %in% c("barometer_mmHg","cond_spec_uScm")) %>%
+  ggplot(aes(x=value, y=depth_m, color=parameter))+
+  geom_point()+
+  scale_y_reverse()+
+  facet_wrap(parameter~., scales="free_x", nrow = 2)+
+  labs(title=paste(unique(LOC14$lake),unique(LOC14$date)))
+
+LOC16 <- process_ysi(LOCfiles[16])
+head(LOC16)
+
+LOC16 %>%
+  mutate(depth_m=round(depth_m/0.25)*0.25) %>% #round to the nearest 0.5
+  group_by(depth_m, parameter, lake) %>%
+  mutate(value = median(value, na.rm=TRUE)) %>%
+  mutate(month=month(date_time)) %>%
+  filter(!parameter %in% c("barometer_mmHg","cond_spec_uScm")) %>%
+  ggplot(aes(x=value, y=depth_m, color=parameter))+
+  geom_point()+
+  scale_y_reverse()+
+  facet_wrap(parameter~., scales="free_x", nrow = 2)+
+  labs(title=paste(unique(LOC16$lake),unique(LOC16$date)))
+
+
+# OTHER LAKES -------------------------------------------------------------
 
 
 #Compile all San Juans profiles 
@@ -20,20 +218,20 @@ SJ_all <- bind_rows(LFM_july,
 SJ_all %>%
   mutate(month=month(date_time)) %>%
   filter(parameter=="temp") %>%
-  ggplot(aes(x=value, y=depth, color=factor(month)))+
+  ggplot(aes(x=value, y=depth_m, color=factor(month)))+
   geom_point()+
   scale_y_reverse()+
   facet_wrap(lake~.)
 
 
-#Can we round the depth values to smooth out the profiles?
+#Can we round the depth_m values to smooth out the profiles?
 SJ_all %>%
-  mutate(depth=round(depth/0.25)*0.25) %>% #round to the nearest 0.25
-  group_by(depth, parameter, lake) %>%
+  mutate(depth_m=round(depth_m/0.25)*0.25) %>% #round to the nearest 0.25
+  group_by(depth_m, parameter, lake) %>%
   mutate(value = median(value, na.rm=TRUE)) %>%
   mutate(month=month(date_time)) %>%
   filter(parameter=="do_mgL") %>%
-  ggplot(aes(x=value, y=depth, color=factor(month)))+
+  ggplot(aes(x=value, y=depth_m, color=factor(month)))+
   geom_point()+
   scale_y_reverse()+
   facet_wrap(lake~.)
@@ -47,7 +245,7 @@ SJ_all %>%
   mutate(value = median(value, na.rm=TRUE)) %>%
   mutate(month=month(date_time)) %>%
   filter(parameter=="do_mgL") %>%
-  ggplot(aes(x=value, y=depth, color=factor(month)))+
+  ggplot(aes(x=value, y=depth_m, color=factor(month)))+
   geom_point()+
   facet_wrap(lake~.)
 #Maybe?
@@ -56,7 +254,7 @@ SJ_all %>%
 LOC_march  %>%
   mutate(month=month(date_time)) %>%
   filter(parameter=="temp") %>%
-  ggplot(aes(x=value, y=depth, color=factor(month)))+
+  ggplot(aes(x=value, y=depth_m, color=factor(month)))+
   geom_point()+
   scale_y_reverse()+
   facet_wrap(lake~.)
@@ -64,12 +262,12 @@ LOC_march  %>%
 
 #Round to smooth out profiles?
 LOC_march %>%
-  mutate(depth=round(depth/0.25)*0.25) %>% #round to the nearest 0.1
-  group_by(depth, parameter, lake) %>%
+  mutate(depth_m=round(depth_m/0.25)*0.25) %>% #round to the nearest 0.1
+  group_by(depth_m, parameter, lake) %>%
   mutate(value = median(value, na.rm=TRUE)) %>%
   mutate(month=month(date_time)) %>%
   filter(parameter=="temp") %>%
-  ggplot(aes(x=value, y=depth, color=factor(month)))+
+  ggplot(aes(x=value, y=depth_m, color=factor(month)))+
   geom_point()+
   scale_y_reverse()+
   facet_wrap(lake~.)
@@ -79,7 +277,7 @@ LOC_march %>%
 GL4_apr  %>%
   mutate(month=month(date_time)) %>%
   filter(parameter=="temp") %>%
-  ggplot(aes(x=value, y=depth, color=factor(month)))+
+  ggplot(aes(x=value, y=depth_m, color=factor(month)))+
   geom_point()+
   scale_y_reverse()+
   facet_wrap(lake~.)
@@ -87,12 +285,12 @@ GL4_apr  %>%
 
 #Round to smooth out profiles?
 GL4_apr %>%
-  mutate(depth=round(depth/0.5)*0.5) %>% #round to the nearest 0.5
-  group_by(depth, parameter, lake) %>%
+  mutate(depth_m=round(depth_m/0.5)*0.5) %>% #round to the nearest 0.5
+  group_by(depth_m, parameter, lake) %>%
   mutate(value = median(value, na.rm=TRUE)) %>%
   mutate(month=month(date_time)) %>%
   filter(parameter=="temp") %>%
-  ggplot(aes(x=value, y=depth, color=factor(month)))+
+  ggplot(aes(x=value, y=depth_m, color=factor(month)))+
   geom_point()+
   scale_y_reverse()+
   facet_wrap(lake~.)+
